faf3669626e3ff8deebb39aecb41ddf09e60e4ee
mattburlage <mattburlage@gmail.com>
2021-01-09 15:38:28 -0500

    added index and stuff

--- planning/models.py
+++ planning/models.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
 
 class MealService(models.Model):
     name = models.CharField(max_length=32)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -16,22 +18,33 @@ class Delivery(models.Model):
     def __str__(self):
         return f'{self.service.name} on {self.arrival.strftime("%x")}'
 
+    class Meta:
+        verbose_name_plural = "Deliveries"
+
 
 MEAL_STATUS_CHOICES = [
-    (0, 'On The Way'),
-    (1, 'Received'),
-    (2, 'Consumed'),
+    ('On The Way', 'On The Way'),
+    ('Received', 'Received'),
+    ('Consumed', 'Consumed'),
 ]
 
 
 class Meal(models.Model):
     name = models.CharField(max_length=64)
-    status = models.CharField(max_length=12, choices=MEAL_STATUS_CHOICES, default=1)
+    delivery = models.ForeignKey(Delivery, on_delete=models.CASCADE)
+    status = models.CharField(max_length=12, choices=MEAL_STATUS_CHOICES, default='On The Way')
+    servings = models.IntegerField(default=1)
     eat_by = models.DateField(null=True, blank=True)
 
     @staticmethod
-    def active_meals():
-        return Meal.objects.filter(eat_by_gte=timezone.now().date(), status=1)
+    def active_meals(active_date=None):
+        if not active_date:
+            active_date = timezone.now().date()
+
+        return Meal.objects.filter(eat_by_gte=active_date, status=1)
+
+    def service(self):
+        return self.delivery.service
 
     def __str__(self):
         return f'{self.name}'
--- planning/views.py
+++ planning/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from planning.models import Meal
+
+
+def index(request):
+    context = {
+        'active_meals': Meal.objects.filter(status='Received'),
+        'selected_meals': Meal.objects.filter(status='On The Way'),
+    }
+    return render(request, 'planning/index.html', context)
